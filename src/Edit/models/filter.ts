44b66fb5d6aee5f8ed27b5631c036fa086e8ca8a
Tanner Walters <r.wisniewski1101@gmail.com>
2023-03-09 22:16:24 -0600

    Start Filtering Model

---
+++ src/Edit/models/filter.ts
@@ -0,0 +1,25 @@
+export enum FilterType {
+  Tags = 'tags',
+  Flavors = 'flavors'
+}
+
+export interface FilterBase<TType extends FilterType> {
+  type: TType;
+  startDate: string | null;
+  endDate: string | null;
+}
+
+export interface FlavorIdRecord {
+  brand: string;
+  flavor: string;
+}
+
+export interface FlavorFilter extends FilterBase<FilterType.Flavors> {
+  flavors: FlavorIdRecord[];
+}
+
+export interface TagFilter extends FilterBase<FilterType.Tags> {
+  tags: string[];
+}
+
+export type FilterDefinition = FlavorFilter | TagFilter;

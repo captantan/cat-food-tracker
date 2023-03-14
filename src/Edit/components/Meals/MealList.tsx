e27675acfb96cfc06618690843921e553fe800e4
Tanner Walters <r.wisniewski1101@gmail.com>
2023-03-14 07:45:53 -0500

    Remove unneeded div from meal list

--- src/Edit/components/Meals/MealList.tsx
+++ src/Edit/components/Meals/MealList.tsx
@@ -9,15 +9,13 @@ export const MealList: React.FC = () => {
 
   return (
     <>
-      <Box sx={{ maxWidth: 960, width: '100%', m: '0 auto' }}>
-        {list.length ? (
-          list.map((date) => <DayCard date={date} key={date.date} />)
-        ) : (
-          <Typography component="p" variant="caption">
-            No meals, yet
-          </Typography>
-        )}
-      </Box>
+      {list.length ? (
+        list.map((date) => <DayCard date={date} key={date.date} />)
+      ) : (
+        <Typography component="p" variant="caption">
+          No meals, yet
+        </Typography>
+      )}
     </>
   );
 };

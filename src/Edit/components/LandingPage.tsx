28aef395eb3fdaf1c8715a80c0b303f10ba7a209
Tanner Walters <r.wisniewski1101@gmail.com>
2023-03-15 23:53:37 -0500

    File landing page revamp

--- src/Edit/components/LandingPage.tsx
+++ src/Edit/components/LandingPage.tsx
@@ -1,10 +1,28 @@
-import { Button, Typography } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardHeader,
+  Typography,
+} from '@mui/material';
+import { SxProps } from '@mui/system';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { CenterBox } from '../../components/CenterBox';
 import { brandsSelectors } from '../store/selectors';
 import { AppHeader } from './AppHeader';
+
+const maxWidth = '360px';
+
+const cardCSS: SxProps = {
+  maxWidth,
+  width: '100%',
+  '&:not(:last-of-type)': {
+    mb: 3,
+  },
+};
 
 export const LandingPage: React.FC = () => {
   const hasFlavors = useSelector(brandsSelectors.hasAnyFlavors);
@@ -14,16 +32,48 @@ export const LandingPage: React.FC = () => {
       <AppHeader title="File opened"></AppHeader>
 
       <CenterBox>
-        <Typography variant="body1">
+        <Typography variant="body1" textAlign="center" sx={{ maxWidth, mb: 3 }}>
           The file has been opened. What would you like to do first?
         </Typography>
-        <Button color="primary" component={Link} to="./brands">
-          Edit Brands
-        </Button>
+
+        <Card sx={cardCSS}>
+          <CardHeader
+            title="Brands"
+            titleTypographyProps={{
+              variant: 'h6',
+            }}
+            sx={{ pb: 0 }}></CardHeader>
+          <CardContent>
+            <Typography>
+              Enter brands and flavors. Add tags to find meals that served food
+              with certain properties.
+            </Typography>
+          </CardContent>
+          <CardActionArea sx={{ textAlign: 'center' }}>
+            <Button color="primary" fullWidth component={Link} to="./brands">
+              Go
+            </Button>
+          </CardActionArea>
+        </Card>
         {hasFlavors && (
-          <Button color="secondary" component={Link} to="./meals">
-            Enter Meals
-          </Button>
+          <Card sx={cardCSS}>
+            <CardHeader
+              title="Meals"
+              titleTypographyProps={{
+                variant: 'h6',
+              }}
+              sx={{ pb: 0 }}></CardHeader>
+            <CardContent>
+              <Typography>
+                Enter meals, add notes, and log the amount that was eaten.
+              </Typography>
+            </CardContent>
+            <CardActionArea sx={{ textAlign: 'center' }}>
+              <Button color="primary" fullWidth component={Link} to="./meals">
+                Go
+              </Button>
+            </CardActionArea>
+          </Card>
         )}
       </CenterBox>
     </>
